21f49e38022c28d2ebdd2aacb1fdb15168db1403
AnotherBread <akib124ab@gmail.com>
2024-10-20 21:47:06 +0600

    Update BookRepository.kt

--- bookRestApi/src/main/kotlin/com/ibrahim/bookRestApi/repository/BookRepository.kt
+++ bookRestApi/src/main/kotlin/com/ibrahim/bookRestApi/repository/BookRepository.kt
@@ -3,7 +3,7 @@ package com.ibrahim.bookRestApi.repository
 import com.ibrahim.bookRestApi.BookModel
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-
+//new teest
 
 @Repository
 interface BookRepository: CrudRepository<BookModel, Int> {
